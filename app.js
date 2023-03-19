013996bdd0cbed683032f040c1c97c83ff642be5
anasahmd <anasahmad0239@gmail.com>
2023-03-19 21:45:09 +0530

    Add user for input for grid size

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const container = document.getElementById('container');
+const customGridBtn = document.getElementById('custom-grid-btn');
 
-let size = 16;
+const gridGenerate = (size) => {
+  container.innerHTML = '';
+  for (let i = 0; i < size * size; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid-item');
+    container.appendChild(div);
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  }
 
-for (let i = 0; i < size * size; i++) {
-  const div = document.createElement('div');
-  div.classList.add('grid-item');
-  container.appendChild(div);
-}
+  const gridItems = document.querySelectorAll('.grid-item');
 
-const gridItems = document.querySelectorAll('.grid-item');
+  for (let gridItem of gridItems) {
+    gridItem.addEventListener('mouseover', () => {
+      gridItem.style.backgroundColor = 'black';
+    });
+  }
+};
 
-for (let gridItem of gridItems) {
-  gridItem.addEventListener('mouseover', () => {
-    console.log('hello');
-    gridItem.style.backgroundColor = 'black';
-  });
-}
+gridGenerate(16);
+
+customGridBtn.addEventListener('click', () => {
+  let size = window.prompt('Enter grid size');
+  if (size <= 100) {
+    gridGenerate(size);
+  } else {
+    window.alert('Maximum size is 100');
+  }
+});
